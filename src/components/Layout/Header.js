433f0867978b2b19eb8eb346746732447d7fbe24
43695512 <yanter@wp.pl>
2021-08-28 18:38:47 +0200

    Header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from "react";
+
+import classes from "./Header.module.css";
+import mealImage from "../../assets/picture.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Foodies</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img
+          src={mealImage}
+          alt="People enjoying their meal at the restaurant"
+        ></img>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
