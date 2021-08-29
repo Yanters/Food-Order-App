409318da2b4774ae11ddbd0a444d30b9fbbb888c
43695512 <yanter@wp.pl>
2021-08-29 19:08:08 +0200

    Cart List Structure

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,12 +4,12 @@ import classes from "./Header.module.css";
 import mealImage from "../../assets/picture.jpg";
 import HeaderCartButton from "./HeaderCartButton";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>Foodies</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img
