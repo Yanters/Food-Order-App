231b3abfdd2c3608d3759b47473f8d4a7f428a0e
43695512 <yanter@wp.pl>
2021-08-30 22:41:01 +0200

    Animations on Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,14 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
+
+  const { totalAmount: totalAmountCtx } = cartCtx;
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
@@ -19,25 +22,47 @@ const Cart = (props) => {
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {cartCtx.items.map((item) => (
+      {cartCtx.items.map((item, index) => (
         <CartItem
           key={item.id}
+          id={index}
           name={item.name}
           amount={item.amount}
           price={item.price}
           onRemove={cartItemRemoveHandler.bind(null, item.id)}
           onAdd={cartItemAddHandler.bind(null, item)}
+          className=""
         />
       ))}
     </ul>
   );
 
+  const totalAmountClasses = btnIsHighlighted ? classes.bump : "";
+
+  useEffect(() => {
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [totalAmountCtx]);
+
+  // console.log(cartItems.props.children.length);
+
   return (
     <Modal onClose={props.onClose}>
-      {cartItems}
+      {cartItems.props.children.length === 0 ? (
+        <p className={classes.noitems}>No items found.</p>
+      ) : (
+        cartItems
+      )}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>{totalAmount}</span>
+        <span className={totalAmountClasses}>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,10 +1,33 @@
-import classes from './CartItem.module.css';
+import { useContext, useEffect, useState } from "react";
+import classes from "./CartItem.module.css";
+import CartContext from "../../store/cart-context";
 
 const CartItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
 
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+  const { amount } = cartCtx.items[props.id];
+
+  // console.log(cartCtx.items[props.id].amount);
+  // console.log(props.id);
+
+  useEffect(() => {
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [amount]);
+
   return (
-    <li className={classes['cart-item']}>
+    <li
+      className={`${classes["cart-item"]} ${btnIsHighlighted && classes.bump}`}
+    >
       <div>
         <h2>{props.name}</h2>
         <div className={classes.summary}>
